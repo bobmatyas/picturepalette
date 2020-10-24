b436b8ea531dfcda025f1847733749dab7125b17
bob <45246438+bobmatyas@users.noreply.github.com>
2020-10-23 20:34:55 -0400

    update color button to use css variables

--- src/components/Color-Button.js
+++ src/components/Color-Button.js
@@ -1,42 +1,21 @@
 import React from 'react';
-import styled from "styled-components";
+import './Color-Button.css';
+
 
 function ColorButton(props) {
 
-    const test = () => console.log(`whoop!`);
-
-    const Button = styled.button`
-        ${ props.color === 'grayscale' ? 'background-image: linear-gradient(135deg, #f6f8f9 0%, #e5ebee 50%, #d7dee3 51%, #f5f7f9 100%);' : ''}
-        background-color: ${props.hex};
-        border: solid 2px #eee;
-        color: ${props.text};
-        font-size: 2rem;
-        font-weight: 900;
-        height: 200px;
-        padding: 25px;
-        text-transform: uppercase;
-        width: 200px;
-
-        :hover {
-            cursor: pointer;
-            border: 10px solid ${props.text};
-            transition: border 0.2s;
-        }
-
-        :focus {
-            border: 10px solid ${props.text};
-            transition: border 0.2s;            
-        }
-    `;
+    let cssProperties = {}
+    cssProperties['--background-color'] = props.hex
+    cssProperties['--text-color'] = props.text
 
     return (
-        <Button 
+        <button className="btn" style={cssProperties}
             onClick={() => test()}
             onKeyPress={() => test()}        
         >
                 
                 {props.color}
-        </Button>
+        </button>
     )
 }
 
