1023469705e6558ed28f66f5ac870d52af6a573b
bob <45246438+bobmatyas@users.noreply.github.com>
2020-12-12 12:22:10 -0500

    update palette display

--- src/components/Palette-Download-Photo.js
+++ src/components/Palette-Download-Photo.js
@@ -1,32 +1,48 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { MdFileDownload } from "react-icons/md";
+import { MdFileDownload, MdInfoOutline } from "react-icons/md";
 import styled from 'styled-components';
 
-const DownloadBox = styled.div`
-    align-items: center;
+const InfoHolder = styled.div`
     background-color: #eee;
+    border-bottom-left-radius: 7px;
+    border-bottom-right-radius: 7px;
+    padding: 15px 0;
+`;
+
+const InfoBox = styled.div`
+    align-items: center;
     display: flex;
+    font-size: 12px;
     justify-content: space-between;
     padding: 5px 15px;
     text-transform: uppercase;  
+    
+    p {
+        margin: 0;
+    }
 `;
 
 const Link = styled.a`
     color: #000;
-    text-decoration: underline;
+    text-decoration: none;
 
-    :hover { text-decoration: none; }
+    :hover { text-decoration: underline; }
 `;
 
 function PalettePhotoDownload({imageURL, imageLarge}) {
 
     return (
-        <DownloadBox>
+        <InfoHolder>
+        <InfoBox>
             <MdFileDownload />
-            <p><Link href={imageLarge} target="_blank" rel="noopener noreferrer">Download Image</Link>
-            <br/><Link href={imageURL} target="_blank" rel="noopener noreferrer">Usage Information</Link></p>
-        </DownloadBox>
+            <p><Link href={imageLarge} target="_blank" rel="noopener noreferrer">Download Image</Link></p>
+        </InfoBox>
+        <InfoBox>
+            <MdInfoOutline />
+            <p><Link href={imageURL} target="_blank" rel="noopener noreferrer">Usage Information</Link></p>
+        </InfoBox>
+        </InfoHolder>
     )
 }
 
