2a846c9eec7ae41a2f0c63a5db3bf2ad1cf71ce1
bob <45246438+bobmatyas@users.noreply.github.com>
2020-12-13 10:49:52 -0500

    fix failing build

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Heading from './Heading';
 import Welcome from './Home-Welcome';
 import ColorSelection from './Color-Selection';
